e0f93f91a6600b54725107ce7ed986d7d910be27
Caelana <sabrinasayasith@gmail.com>
2017-09-22 12:30:14 -0400

    Connected UI To Code
    text heading updates when user enters text

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -7,13 +7,33 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
+        //When a ViewController instance is loaded, it sets itself as the delegate of its nameTextField property.
     }
-
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true //always want to respond to user pressing Return key
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //do something with the info entered into the text field
+        mealNameLabel.text = textField.text
+    }
+    
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
 }
 
